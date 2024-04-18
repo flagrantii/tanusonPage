038019334e4af3e8f5513379df21acc0cc36f3c8
flagrantii <tanuson679@gmail.com>
2024-04-18 15:42:08 +0700

    Update company name in CareerPath component

--- app/Port/page.tsx
+++ app/Port/page.tsx
@@ -75,10 +75,10 @@ const CareerPath = () => {
               <div className="mx-auto max-w-6xl px-6 lg:px-8 ">
                 <div className="bg-gray-100 p-4 rounded-lg ">
                   <div className="flex flex-col md:flex-row items-center md:items-start gap-4 sm:py-6 ">
-                    <img src='ever.jpg'className="w-16 h-16 md:w-40 md:h-24 rounded-full bg-gray-200"></img>
+                    <div className="w-16 h-16 md:w-40 md:h-24 rounded-full bg-gray-200"></div>
                     <div className='grid grid-cols-1 gap-1'>
                       <p className="text-gray-500">August 2023 - November 2023</p>
-                      <h3 className="text-lg font-semibold">Ever Medical Technologies</h3>
+                      <h3 className="text-lg font-semibold">Some Software House</h3>
                       
                       <div className='lg:flex gap-6'>
                         <p className="text-gray-600">FullStack Developer</p>
