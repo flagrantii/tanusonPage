4fe8ecac5110f4b878025208f456ec7d665ad6f5
flagrantii <tanuson679@gmail.com>
2025-08-08 16:12:40 +0700

    Update certificate data structure in cert.ts, adding new certificates with detailed descriptions, links, and images. Remove outdated image files and add new background and icon images to enhance visual elements across the application.

--- Data/cert.ts
+++ Data/cert.ts
@@ -11,46 +11,60 @@ export type CertItem = {
 }
 
 export const cers = [
-    {
-      id: 1,
-      title: 'Supervised Machine Learning: Regression and Classification',
-      href: '#',
-      description:
-        'Principles of supervised machine learning By in-depth look at regression and classification methods. Gain practical skills in model training and evaluation. Learn to use algorithms for predictive analytics.',
-      date: 'October 2022',
-      datetime: '2020-03-16',
-      category: { title: 'Coursera', org: 'DeepLearning.AI' },
-        author: {
-        href: '#',
-        imageUrl:
-            '/coursera.png',
-        cersimage: '/cer_deepai.jpeg',
-    },
-      skills: ["Linear Regression", 
-      "Regularization to Avoid Overfitting", 
-      "Logistic Regression for Classification",
-      "Gradient Descent",
-      "Supervised Learning",
-    ],
-    },
-     {
-      id: 2,
-      title: 'Technical Support Fundamentals',
-      href: 'https://www.coursera.org/account/accomplishments/verify/GN3CPHQV377T?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course',
-      description:
-        'gain expertise in Technical Support Fundamentals, mastering troubleshooting, effective customer communication, and problem-solving techniques. Develop a strong foundation in hardware and software support',
-      date: 'October 2022',
-      datetime: '2020-03-16',
-      category: { title: 'Coursera', org: 'Google' },
-      style: 'relative z-10 rounded-full bg-green-200 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100',
+  {
+    id: 1,
+    title: 'Supervised Machine Learning: Regression and Classification',
+    href: 'https://coursera.org/share/1c1244c494a980fd62448707b52a2cab',
+    description:
+      'Principles of supervised machine learning By in-depth look at regression and classification methods. Gain practical skills in model training and evaluation. Learn to use algorithms for predictive analytics.',
+    date: 'October 2022',
+    datetime: '2020-03-16',
+    category: { title: 'Coursera', org: 'DeepLearning.AI' },
       author: {
-        name: 'PCSHS-NST Team',
-        role: 'Frontend Developer',
-        href: '#',
-        imageUrl:
-          '/coursera.png',
-        cersimage: '/cer_googleit.jpeg',
-      },
-      skills: ["Binary Code", "Customer Support", "Linux","Troubleshooting"],
+      href: '#',
+      imageUrl:
+          '/icon/coursera.png',
+      cersimage: '/cert/cer_deepai.jpeg',
+  },
+    skills: ["Linear Regression", 
+    "Regularization to Avoid Overfitting", 
+    "Logistic Regression for Classification",
+    "Gradient Descent",
+    "Supervised Learning",
+  ],
+  },
+   {
+    id: 2,
+    title: 'Technical Support Fundamentals',
+    href: 'https://coursera.org/share/116e66fe61fc085dd32998deaf5dfabe',
+    description:
+      'gain expertise in Technical Support Fundamentals, mastering troubleshooting, effective customer communication, and problem-solving techniques. Develop a strong foundation in hardware and software support',
+    date: 'October 2022',
+    datetime: '2020-03-16',
+    category: { title: 'Coursera', org: 'Google' },
+    author: {
+      href: '#',
+      imageUrl:
+        '/icon/coursera.png',
+      cersimage: '/cert/cer_googleit.jpeg',
     },
+    skills: ["Binary Code", "Customer Support", "Linux","Troubleshooting"],
+  },
+  {
+    id: 3,
+    title: 'Build LookML Objects in Looker',
+    href: 'https://www.cloudskillsboost.google/public_profiles/4070637f-c92e-424f-a35e-c071938698cf/badges/9421029',
+    description:
+      'Certificate in "Build LookML Objects in Looker" validates proficiency in constructing LookML models within Looker, empowering data analysts to create robust data models for business intelligence.',
+    date: 'June 2024',
+    datetime: '2020-03-16',
+    category: { title: 'Google', org: 'Google Cloud' },
+    author: {
+      href: '#',
+      imageUrl:
+        '/icon/google1.jpg',
+      cersimage: '/cert/cer_lookml.png',
+    },
+    skills: ["ML", "AI", "LookML","SQL","Data Analysis","Data Visualization"],
+  },
   ]
